b21d2e6b6d8c0e3f2421c188e46acf69c7ef363f
Kyle Marienthal <kylemarienthal@gmail.com>
2018-01-21 00:00:10 -0600

    bullets added

--- space_invaders/sketch.js
+++ space_invaders/sketch.js
@@ -1,11 +1,11 @@
 var ship;
 var aliens = [];
-var bullet;
+var bullets = [];
 
 function setup(){
   createCanvas(600, 400);
   ship = new Ship();
-  bullet = new Bullet(width/2, height/2);
+  // bullet = new Bullet(width/2, height/2);
   for (var i = 0; i < 6; i++) {
     aliens[i] = new Alien(i*80+80, 60);
   }
@@ -14,14 +14,27 @@ function setup(){
 function draw() {
   background(51);
   ship.show();
-  bullet.show();
-  bullet.move();
+  for (var i = 0; i < bullets.length; i++) {
+    bullets[i].show();
+    bullets[i].move();
+    for (var j = 0; j < aliens.length; j++) {
+      if (bullet[i].hits(aliens[j])){
+        console.log("CONNECTION");
+      }
+
+  }
+
+
   for (var i = 0; i < aliens.length; i++) {
-    aliens[i].show()
+    aliens[i].show();
   }
 }
 
 function keyPressed() {
+  if (key === " "){
+    var bullet = new Bullet(ship.x, height);
+    bullets.push(bullet);
+  }
   if (keyCode === RIGHT_ARROW) {
     ship.move(1);
   } else if (keyCode === LEFT_ARROW){
